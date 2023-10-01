751b6818d230fd5b801c1e93923fc0ffb65cfb05
DimLymp <jimlibo13@gmail.com>
2023-10-01 14:10:16 +0300

    added utility functions

---
+++ source_code/utils.py
@@ -0,0 +1,14 @@
+"""
+Created October 1, 2022,
+author: Dimitris Lymperopoulos
+Description: A file containing some utility functions
+"""
+
+
+def logger_info(message):   # function to simply print the message
+    print("[INFO]: " + message)
+
+
+def logger_error(message):  # function that prints the error message and then exits
+    print("[ERROR]: " + message)
+    exit(-1)
